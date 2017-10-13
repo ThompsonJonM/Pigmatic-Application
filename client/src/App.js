ca1fa364b05ea129d20bab62febb5c21d9ce0704
unknown <thompson.jonm@gmail.com>
2017-10-13 10:11:32 -0500

    footer and header elements added to app, app nested inside index

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import UserForm from './components/forms/UserForm';
+import ViewUsers from './components/data/ViewUsers';
+import Header from './components/site-elements/Header';
+import Footer from './components/site-elements/Footer';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <div className='container-fluid'>
+        <Header />
+
+        <Router>
+          <div>
+              <Route path='/add-new-user' component={ UserForm } />
+              <Route path='/view-users' component={ ViewUsers } />
+          </ div>
+        </ Router>
+
+        <Footer />
+      </ div>
     );
   }
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
-import UserForm from './components/forms/UserForm';
-import ViewUsers from './components/data/ViewUsers';
+// import UserForm from './components/forms/UserForm';
+// import ViewUsers from './components/data/ViewUsers';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Router>
-        <div className='container'>
-            <Route exact path='/' component={ App } />
-            <Route path='/add-new-user' component={ UserForm } />
-            <Route path='/view-users' component={ ViewUsers } />
-        </ div>
-    </ Router>,
+    <App />,
 
     document.getElementById('root')
 );
